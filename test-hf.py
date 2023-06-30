61092e513da1bc8d5ef1f17264f02825bb6cf4b7
wangruohui <12756472+wangruohui@users.noreply.github.com>
2023-06-30 14:54:12 +0800

    rename unit test

--- test-hf.py
+++ test-hf.py
@@ -9,8 +9,14 @@ def init_model():
     torch.set_default_tensor_type(torch.cuda.HalfTensor)
 
     timehere()
-    model_id = "decapoda-research/llama-7b-hf"
-    model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.float16)
+    model_id = "/nvme/wangruohui/llama-65b-hf"
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id, torch_dtype=torch.float16, device_map="auto"
+    )
     timehere("load model")
 
     return model
+
+
+if __name__ == "__main__":
+    model = init_model()
--- test-hf_ds.py
+++ test-hf_ds.py
@@ -7,16 +7,16 @@ def init_model():
     from transformers import AutoModelForCausalLM
     import deepspeed
 
-    torch.set_default_tensor_type(torch.cuda.HalfTensor)
+    # torch.set_default_tensor_type(torch.cuda.HalfTensor)
 
     t()
-    model_id = "decapoda-research/llama-7b-hf"
+    model_id = "/nvme/wangruohui/llama-65b-hf"
     model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.float16)
     t("load model")
 
     ds_model = deepspeed.init_inference(
         model=model,  # Transformers models
-        mp_size=1,  # Number of GPU
+        mp_size=8,  # Number of GPU
         dtype=torch.float16,  # dtype of the weights (fp16)
         replace_method="auto",  # Lets DS autmatically identify the layer to replace
         replace_with_kernel_inject=True,  # replace the model with the kernel injector
@@ -25,3 +25,7 @@ def init_model():
     print(f"model is loaded on device {ds_model.module.device}")
 
     return ds_model
+
+
+if __name__ == "__main__":
+    model = init_model()
